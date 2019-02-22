fd96c9668a96187bc051d9eb94066df79fd44b0c
RenYL <Ren.Y.L@hotmail.com>
2019-02-21 22:04:45 -0800

    Meal Vector

--- MLFile/Script2.js
+++ MLFile/Script2.js
@@ -1,7 +1,7 @@
-function setup() {
-  var c = createCanvas(100, 100);
-  background(200);
-  textAlign(CENTER);
-  text('drop image', width / 2, height / 2);
-  c.drop(file => loadImage(file.data, img => image(img, 0, 0, width, height)));
+function setup(){
+    noCanvas();
+    const data = tf.tensor([64,64,255,255,255]);
+    data.print();
+    console.log("hello");
+    
 }
